6d9c508843927d0606f71ceca33c8cbf223123d0
Basile Dyment <basaile92@gmail.com>
2024-02-25 21:36:44 +0100

    Modify launch create handler

---
+++ src/lambda.ts
@@ -0,0 +1,6 @@
+import schema from "./modules/index";
+import {ApolloServer} from "apollo-server-lambda";
+
+
+const server = new ApolloServer({ schema });
+export const handler = server.createHandler();
