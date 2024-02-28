2efb1cc0420b6be39aa94789d18065af2dc7156d
Basile Dyment <basaile92@gmail.com>
2024-02-28 18:11:50 +0100

    Fix sonar error

--- modules/help/queries/helpByCommand.js
+++ modules/help/queries/helpByCommand.js
@@ -3,7 +3,7 @@ import commands from "../../../data/commands.js";
 export default async (root, { commandLabel }) => {
     const command = commands
         .find((cmd) =>
-            cmd.command == commandLabel
+            cmd.command === commandLabel
         );
     if(command) {
         command.usages.sort((usage1, usage2) =>
