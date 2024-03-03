9dafcb9569f8c9a27c625f656aad57aff590d2b5
Basile Dyment <basaile92@gmail.com>
2024-03-03 14:06:16 +0100

    Remove babel on deploy artefact and transform ES6 import json in require commonJs import json

--- src/client/DataClient.js
+++ src/client/DataClient.js
@@ -1,13 +1,13 @@
-const availability = '../../data/availability.json';
-const certificates = '../../data/certificates.json';
-const commands = '../../data/commands.json';
-const companies = '../../data/companies.json';
-const description = '../../data/description.json';
-const hobbies = '../../data/hobbies.json';
-const languages = '../../data/languages.json';
-const missions = '../../data/missions.json';
-const projects = '../../data/projects.json';
-const studies = '../../data/studies.json';
+const availability = require('../../data/availability.json');
+const certificates = require('../../data/certificates.json');
+const commands = require('../../data/commands.json');
+const companies = require('../../data/companies.json');
+const description = require('../../data/description.json');
+const hobbies = require('../../data/hobbies.json');
+const languages = require('../../data/languages.json');
+const missions = require('../../data/missions.json');
+const projects = require('../../data/projects.json');
+const studies = require('../../data/studies.json');
 
 export class DataClient {
   getAvailability = () => {
