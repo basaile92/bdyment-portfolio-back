e8b7d9ecd7fd80fe76da3ace5c0e4f2377993662
Basile Dyment <basaile92@gmail.com>
2024-03-03 16:15:44 +0100

    Readd Time scalar to fix bug

--- src/graphql/elements/BaseGraphQLElements.js
+++ src/graphql/elements/BaseGraphQLElements.js
@@ -4,12 +4,25 @@ import { AbstractGraphQLElements } from './AbstractGraphQLElements.js';
 export class BaseGraphQLElements extends AbstractGraphQLElements {
   constructor() {
     const baseTypeDefs = gql`
+      scalar Time
+
       type Query {
         getVersion: String!
       }
+
+      type Mutation {
+        version: String!
+      }
     `;
 
+    const timeScalar = new GraphQLScalarType({
+      name: 'Time',
+      description: 'Time custom scalar type',
+      serialize: (value) => value,
+    });
+
     const baseResolvers = {
+      Time: timeScalar,
       Query: {},
     };
     super(baseTypeDefs, baseResolvers);
