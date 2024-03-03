925e3b9f69fc2fe4aad6b318fa6a0bc429774264
Basile Dyment <basaile92@gmail.com>
2024-03-03 13:47:19 +0100

    Transpile import because lambda doesn't accept import json

--- src/client/DataClient.js
+++ src/client/DataClient.js
@@ -1,13 +1,13 @@
-import availability from '../../data/availability.json' with { type: 'json' };
-import certificates from '../../data/certificates.json' with { type: 'json' };
-import commands from '../../data/commands.json' with { type: 'json' };
-import companies from '../../data/companies.json' with { type: 'json' };
-import description from '../../data/description.json' with { type: 'json' };
-import hobbies from '../../data/hobbies.json' with { type: 'json' };
-import languages from '../../data/languages.json' with { type: 'json' };
-import missions from '../../data/missions.json' with { type: 'json' };
-import projects from '../../data/projects.json' with { type: 'json' };
-import studies from '../../data/studies.json' with { type: 'json' };
+import availability from '../data/availability.json' with { type: 'json' };
+import certificates from '../data/certificates.json' with { type: 'json' };
+import commands from '../data/commands.json' with { type: 'json' };
+import companies from '../data/companies.json' with { type: 'json' };
+import description from '../data/description.json' with { type: 'json' };
+import hobbies from '../data/hobbies.json' with { type: 'json' };
+import languages from '../data/languages.json' with { type: 'json' };
+import missions from '../data/missions.json' with { type: 'json' };
+import projects from '../data/projects.json' with { type: 'json' };
+import studies from '../data/studies.json' with { type: 'json' };
 
 export class DataClient {
   getAvailability = () => {
