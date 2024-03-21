313ad23aa0d290c2dc9d1819243058b3ccd4bfb3
Basile Dyment <basaile92@gmail.com>
2024-03-21 10:25:03 +0100

    Add new certificate

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -5,4 +5,10 @@ export const certificates = [
     year: '2023',
     link: 'https://www.codingame.com/certification/fElMpIblrdDfAPq7V0G8CA',
   },
+  {
+    certifier: 'Refactoring Guru',
+    skill: 'Software craftsmanship',
+    year: '2024',
+    link: 'https://refactoring.guru/cert/r/MTY0MTMy',
+  }
 ];
